7afe2f71c3d50b7be96d1609ffa3ff0ec21ad874
unan25 <kunan25@naver.com>
2023-10-13 12:37:00 +0900

    config route 설정법

--- apigateway-server/src/main/java/com/example/apigatewayserver/config/FilterConfig.java
+++ apigateway-server/src/main/java/com/example/apigatewayserver/config/FilterConfig.java
@@ -10,7 +10,9 @@ public class FilterConfig {
     
     // 게이트웨이를 통해 들어갔을 때만 필터가 적용
     // ex) localhost8000 으로 들어가야 적용
-    @Bean
+
+    // Bean만 주석 처리하면 해당 메서드가 리턴하는 빈이 적재되지 ㅇ낳으므로 설정도 초기화됨
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(r -> r.path("/first-service/**")
--- first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
+++ first-service/src/main/java/com/playdata/firstservice/controller/FirstserviceController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("first-service")// gateway서버의 predicates의 식별 주소를 추가해야 한다.
 public class FirstserviceController {
 
-//    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST}) // Get, Post 두 가지의 역할 가능
+//    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST}) // Get, Post 두 가지의 역할 가능하다는 예시
+    
      @RequestMapping(value = "hello", method = RequestMethod.GET)
 //    @GetMapping
     public String hello() {
