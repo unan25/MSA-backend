0000f9bdbc1c86b798ebb68434b52770de3c56b9
unan25 <kunan25@naver.com>
2023-10-16 10:52:55 +0900

    디스커버리 클라이언트

--- discovery-server/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.example.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // 이 어노테이션을 활용해야 유레카 서버 수집 및 대시보드 동작
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
