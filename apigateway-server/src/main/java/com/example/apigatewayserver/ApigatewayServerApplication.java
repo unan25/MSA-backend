cded906b6a2f16fd0a7f44a2874fc85fffed5c92
unan25 <kunan25@naver.com>
2023-10-16 15:04:22 +0900

    유레카 적용법

--- apigateway-server/src/main/java/com/example/apigatewayserver/ApigatewayServerApplication.java
+++ apigateway-server/src/main/java/com/example/apigatewayserver/ApigatewayServerApplication.java
@@ -3,7 +3,9 @@ package com.example.apigatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableEurekaServer
 public class ApigatewayServerApplication {
 
     public static void main(String[] args) {
