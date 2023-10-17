d7f0de74dfcdae7aa222a4fe4028a7c9d13bb666
unan25 <kunan25@naver.com>
2023-10-17 09:50:14 +0900

    회원가입 및 조회기능 구현 예제

---
+++ user-service/src/main/java/com/example/userservice/user/RequestCreateDTO.java
@@ -0,0 +1,8 @@
+package com.example.userservice.user;
+
+public class RequestCreateDTO {
+
+    String email;
+    String pw;
+    String name;
+}
---
+++ user-service/src/main/java/com/example/userservice/user/UserEntity.java
@@ -0,0 +1,8 @@
+package com.example.userservice.user;
+
+public class UserEntity {
+
+    private Long id;
+    private String email;
+    private String name;
+}
