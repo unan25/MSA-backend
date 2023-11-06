ad583d40f9927d53c7f94e9b25232a8508424f4f
unan25 <kunan25@naver.com>
2023-11-06 12:40:55 +0900

    메시지 큐 추출 및 DB 저장

--- item-service/src/main/java/com/example/itemservice/util/Consumer.java
+++ item-service/src/main/java/com/example/itemservice/util/Consumer.java
@@ -1,6 +1,7 @@
 package com.example.itemservice.util;
 
 import com.example.itemservice.dto.RequestCreateItemDto;
+import com.example.itemservice.service.ItemService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +14,20 @@ public class Consumer {
 
     private final ObjectMapper objectMapper;
 
+    private final ItemService itemService;
+
+//    @RabbitListener(queues = "ITEM_CREATE_QUEUE")
+//    public String getTestMessage(String message) throws JsonProcessingException {
+//        // 메시지에 심는것
+//        System.out.println("큐에서 뽑아낸 메시지 : " + message);
+//        return message;
+//    }
+
     @RabbitListener(queues = "ITEM_CREATE_QUEUE")
-    public String getTestMessage(String message) throws JsonProcessingException {
-        // 메시지에 심는것
-        System.out.println("큐에서 뽑아낸 메시지 : " + message);
-        return message;
+    public void createItem(String message) throws JsonProcessingException{
+        //ObjectMapper.readValue("String 형식인 Json", 목적 객체.class);
+        RequestCreateItemDto requestCreateItemDto = objectMapper.readValue(message, RequestCreateItemDto.class);
+        // service에서 DTO를 입력받아 DB에 INSERT해주는 로직을 호출해주세요.
+        itemService.createItem(requestCreateItemDto);
     }
 }
